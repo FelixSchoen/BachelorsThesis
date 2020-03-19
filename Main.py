544aed4c44453245e1d81b9e9e4a64ca9759e685
Felix Schön <felix.schoen@student.tuwien.ac.at>
2020-03-19 19:27:46 +0100

    Implement complexity

--- Main.py
+++ Main.py
@@ -1,19 +1,28 @@
 from mido import MidiFile, MetaMessage
-
+from os import walk
 import Music as mu
 from Music import Sequence, MessageType, Element
 
 
 def main():
-    musical = mu.Musical.from_midi_file(MidiFile("res/beethoven_op27_r.mid"), MidiFile("res/beethoven_op27_l.mid")).to_midi_file()
+    for (dirpath, dirnames, filenames) in walk("res"):
+        for name in filenames:
+            print(name)
+            midi_file = MidiFile(dirpath + "/" + name)
+            sequence = mu.Sequence.from_midi_file(midi_file)
+            print(Sequence.average_complexity(sequence.split(sequence.numerator, sequence.denominator)))
+
 
 def test():
-    seq = mu.Sequence.from_midi_file(MidiFile("res/beethoven_op27_csmin_mo1_0.mid"))
-    print(seq.detect_scale()[0])
-    print(seq.detect_scale()[1])
-    mu.Sequence.stitch_together(seq.split(seq.numerator, seq.denominator)).to_midi_file().save("out/stitched.mid")
+    seq = Sequence.from_midi_file(MidiFile("res/4-4/beethoven_op27_csmin_mo3_0.mid"))
+    hash = set()
+    for element in seq.elements:
+        if not element.message_type == MessageType.wait:
+            continue
+        hash.add(element)
+    print(hash)
+
 
 
 if __name__ == '__main__':
-    #main()
     test()
--- Music.py
+++ Music.py
@@ -17,6 +17,10 @@ def print_midi_file(midi_file: MidiFile, amount=-1) -> None:
 
 def print_meta_midi_file(midi_file: MidiFile) -> None:
     print("Ticks per beat: " + str(midi_file.ticks_per_beat) + ", Type: " + str(midi_file.type))
+    for j, track in enumerate(midi_file.tracks):
+        for i, message in enumerate(track):
+            if isinstance(message, MetaMessage):
+                print("Track " + str(j) + ": " + message.__str__())
 
 
 class Note(Enum):
@@ -60,18 +64,25 @@ class Note(Enum):
 
 
 class Scale(Enum):
-    cmaj_amin = {Note.c, Note.d, Note.e, Note.f, Note.g, Note.a, Note.b}
-    gmaj_emin = {Note.g, Note.a, Note.b, Note.c, Note.d, Note.e, Note.f_s}
-    dmaj_bmin = {Note.d, Note.e, Note.f_s, Note.g, Note.a, Note.b, Note.c_s}
-    amaj_fsmin = {Note.a, Note.b, Note.c_s, Note.d, Note.e, Note.f_s, Note.g_s}
-    emaj_csmin = {Note.e, Note.f_s, Note.g_s, Note.a, Note.b, Note.c_s, Note.d_s}
-    bmaj_gsmin = {Note.b, Note.c_s, Note.d_s, Note.e, Note.f_s, Note.g_s, Note.a_s}
-    fmaj_dmin = {Note.f, Note.g, Note.a, Note.b_b, Note.c, Note.d, Note.e}
-    bbmaj_gmin = {Note.b_b, Note.c, Note.d, Note.e_b, Note.f, Note.g, Note.a}
-    ebmaj_cmin = {Note.e_b, Note.f, Note.g, Note.a_b, Note.b_b, Note.c, Note.d}
-    abmaj_fmin = {Note.a_b, Note.b_b, Note.c, Note.d_b, Note.e_b, Note.f, Note.g}
-    dbmaj_bbmin = {Note.d_b, Note.e_b, Note.f, Note.g_b, Note.a_b, Note.b_b, Note.c}
-    fsmaj_dsmin = {Note.f_s, Note.g_s, Note.a_s, Note.b, Note.c_s, Note.d_s, Note.f}  # Special
+    cmaj_amin = [Note.c, Note.d, Note.e, Note.f, Note.g, Note.a, Note.b]
+    gmaj_emin = [Note.g, Note.a, Note.b, Note.c, Note.d, Note.e, Note.f_s]
+    dmaj_bmin = [Note.d, Note.e, Note.f_s, Note.g, Note.a, Note.b, Note.c_s]
+    amaj_fsmin = [Note.a, Note.b, Note.c_s, Note.d, Note.e, Note.f_s, Note.g_s]
+    emaj_csmin = [Note.e, Note.f_s, Note.g_s, Note.a, Note.b, Note.c_s, Note.d_s]
+    bmaj_gsmin = [Note.b, Note.c_s, Note.d_s, Note.e, Note.f_s, Note.g_s, Note.a_s]
+    fmaj_dmin = [Note.f, Note.g, Note.a, Note.b_b, Note.c, Note.d, Note.e]
+    bbmaj_gmin = [Note.b_b, Note.c, Note.d, Note.e_b, Note.f, Note.g, Note.a]
+    ebmaj_cmin = [Note.e_b, Note.f, Note.g, Note.a_b, Note.b_b, Note.c, Note.d]
+    abmaj_fmin = [Note.a_b, Note.b_b, Note.c, Note.d_b, Note.e_b, Note.f, Note.g]
+    dbmaj_bbmin = [Note.d_b, Note.e_b, Note.f, Note.g_b, Note.a_b, Note.b_b, Note.c]
+    fsmaj_dsmin = [Note.f_s, Note.g_s, Note.a_s, Note.b, Note.c_s, Note.d_s, Note.f]  # Special
+
+    @staticmethod
+    def from_tonic(note: Note):
+        switcher = {
+            Note.c: Scale.cmaj_amin
+        }
+        # TODO
 
 
 class MessageType(Enum):
@@ -150,6 +161,22 @@ class Sequence:
         sequence.denominator = denominator
         return sequence
 
+    @staticmethod
+    def average_complexity(sequences: list):
+        complexity = list()
+        occurrences = 0
+        for sequence in sequences:
+            complexity.append(sequence.complexity_breakdown())
+            occurrences += 1
+
+        if occurrences == 0:
+            occurrences = 1
+        avg_complexity = sum([pair[0] for pair in complexity]) / occurrences
+        dict = {"Note Values": sum([pair[1]["Note Values"] for pair in complexity]) / occurrences,
+                "Note Classes": sum([pair[1]["Note Classes"] for pair in complexity]) / occurrences,
+                "Concurrent Notes": sum([pair[1]["Concurrent Notes"] for pair in complexity]) / occurrences}
+        return avg_complexity, dict
+
     def to_midi_track(self) -> MidiTrack:
         track = MidiTrack()
 
@@ -188,15 +215,13 @@ class Sequence:
         midi_file.tracks.append(self.to_midi_track())
         return midi_file
 
-    def detect_scale(self) -> (list, float):
+    def detect_ideal_scale(self) -> (list, list):
         mismatch = dict()
 
         for scale in Scale:
             mismatch[scale] = 0
 
         for element in self.elements:
-            if element.message_type != MessageType.play:
-                continue
             note_value = element.value % 12
             note = Note.from_note_value(note_value)
             for scale in Scale:
@@ -206,7 +231,7 @@ class Sequence:
         guess = sorted(mismatch.items(), key=lambda item: item[1])
         percentage = list(map(lambda x: 1 - x[1] / len(self.elements), guess))
 
-        return sorted(mismatch.items(), key=lambda item: item[1])[0][0], percentage[0]
+        return sorted(mismatch.items(), key=lambda item: item[1]), percentage
 
     def transpose(self, steps: int):
         for i, element in enumerate(self.elements):
@@ -290,6 +315,108 @@ class Sequence:
         seq_list.append(seq)
         return seq_list
 
+    def complexity(self):
+        return self.complexity_breakdown()[0]
+
+    def complexity_breakdown(self):
+        complex_note_values = self.__complexity_note_values()
+        weight_note_values = 7
+        complex_note_classes = self.__complexity_note_classes()
+        weight_note_classes = 6
+        complex_concurrent_notes = self.__complexity_concurrent_notes()
+        weight_concurrent_notes = 3
+
+        weight_sum = weight_note_values + weight_note_classes + weight_concurrent_notes
+        complexity = (weight_note_values / weight_sum * complex_note_values) + \
+                     (weight_note_classes / weight_sum * complex_note_classes) + \
+                     (weight_concurrent_notes / weight_sum * complex_concurrent_notes)
+
+        dict = {"Note Values": complex_note_values,
+                "Note Classes": complex_note_classes,
+                "Concurrent Notes": complex_concurrent_notes}
+
+        return complexity, dict
+
+    def __complexity_note_values(self):
+        time = 0
+        occurrences = 0
+
+        for element in self.elements:
+            if element.message_type == MessageType.wait:
+                time += element.value
+                occurrences += 1
+        average_time = time / occurrences if occurrences else time
+
+        x = average_time
+        value = 6 - 0.1 * x + 8.5E-4 * x ** 2 - 2.425E-6 * x ** 3
+        return Sequence.util_adjust_rating(value)
+
+    def __complexity_note_classes(self):
+        classes = set()
+
+        for element in self.elements:
+            if element.message_type == MessageType.play:
+                classes.add(element.value)
+
+        x = len(classes)
+        value = 1 / 3 * x + 1 / 3
+        return Sequence.util_adjust_rating(value)
+
+    def __complexity_note_amount(self):
+        amount = 0
+        for element in self.elements:
+            if element.message_type == MessageType.play:
+                amount += 1
+
+        x = amount
+        value = -0.1 + 3.25E-1 * x - 1.0E-2 * x ** 2 + 1.5E-4 * x ** 3
+        return Sequence.util_adjust_rating(value)
+
+    def __complexity_concurrent_notes(self):
+        notes = 0
+        occurrences = 0
+        last_element = None
+
+        for element in self.elements:
+            if element.message_type == MessageType.play:
+                notes += 1
+                if last_element is None or not last_element.message_type == MessageType.play:
+                    occurrences += 1
+            last_element = element
+
+        x = notes / occurrences if occurrences else notes
+        value = 5 - 1.65E1 * x + 1.925E1 * x ** 2 - 8 * x ** 3 + 1.15 * x ** 4
+        value *= self.__complexity_note_amount() / 3
+        return Sequence.util_adjust_rating(value)
+
+    def util_first_notes(self, steps: int) -> list:
+        first_notes = list()
+
+        for element in self.elements:
+            if element.message_type == MessageType.wait and len(first_notes) >= steps:
+                return first_notes
+            if element.message_type == MessageType.play:
+                first_notes.append(Note.from_note_value(element.value % 12))
+
+        return first_notes
+
+    def util_last_notes(self, steps: int) -> list:
+        last_notes = list()
+
+        elements = self.elements.copy()
+        elements.reverse()
+        for element in elements:
+            if element.message_type == MessageType.wait and len(last_notes) >= steps:
+                return last_notes
+            if element.message_type == MessageType.play:
+                last_notes.append(Note.from_note_value(element.value % 12))
+
+        return last_notes
+
+    @staticmethod
+    def util_adjust_rating(value: float):
+        return min(5, max(1, value))
+
     def __str__(self) -> str:
         return "(Numerator: " + str(self.numerator) + ", Denominator: " + str(
             self.denominator) + ", Elements: " + str(self.elements) + ")"
@@ -352,7 +479,7 @@ class Musical:
         return midi_file
 
     def detect_scale(self):
-        return self.right_hand.detect_scale(), self.left_hand.detect_scale()
+        return self.right_hand.detect_ideal_scale(), self.left_hand.detect_ideal_scale()
 
     def transpose(self, steps: int):
         self.right_hand.transpose(steps)
@@ -372,6 +499,14 @@ class Element:
 
     def __repr__(self) -> str:
         return self.__str__()
+
+    def to_neuron_representation(self):
+        if self.message_type == MessageType.wait:
+            return 0
+        elif self.message_type == MessageType.play:
+            return self.value - 20
+        elif self.message_type == MessageType.stop:
+            return self.value - 20 + 88
 
 
 class WrappedElement:
